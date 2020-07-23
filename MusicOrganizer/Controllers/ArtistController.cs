37842114e55487d8e182ed9cce212a6c3e515ea4
Megan Hepner <megan.hepner@gmail.com>
2020-07-23 14:37:07 -0700

    add view Artist list to Artist Controller
    <pagrimm@gmail.com>
    <micheal.hansen@gmail.com>

---
+++ MusicOrganizer/Controllers/ArtistController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using MusicOrganizer.Models;
+using System.Collections.Generic;
+
+namespace MusicOrganizer.Controllers
+{
+  public class ArtistsController : Controller
+  {
+    [HttpGet("/artists")]
+    public ActionResult Index()
+    {
+      List<Artist> artistList = Artist.GetAll();
+      return View(artistList);
+    }
+  }
+}

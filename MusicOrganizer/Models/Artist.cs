86cf559763821993c2862dcd21bf39885a80ce16
Megan Hepner <megan.hepner@gmail.com>
2020-07-23 10:34:11 -0700

    pass test Artist class can create new Artist object

--- MusicOrganizer/Models/Artist.cs
+++ MusicOrganizer/Models/Artist.cs
@@ -2,33 +2,13 @@ using System.Collections.Generic;
 
 namespace MusicOrganizer.Models
 {
-  public class Item
+  public class Artist
   {
-    public string Description { get; set; }
-    public int Id { get; }
-    private static List<Item> _instances = new List<Item> {};
+    public string ArtistName { get; set; }
 
-    public Item (string description)
+    public Artist(string artistName)
     {
-      Description = description;
-      _instances.Add(this);
-      Id = _instances.Count;
+      ArtistName = artistName;
     }
-
-    public static List<Item> GetAll()
-    {
-      return _instances;
-    }
-
-    public static void ClearAll()
-    {
-      _instances.Clear();
-    }
-
-    public static Item Find(int searchId)
-    {
-      return _instances[searchId-1];
-    }
-
   }
 }

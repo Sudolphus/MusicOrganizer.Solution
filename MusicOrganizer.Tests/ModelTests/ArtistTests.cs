0d4ee789827f311fbd4fb7ddbec33d711a8be494
Megan Hepner <megan.hepner@gmail.com>
2020-07-23 10:57:41 -0700

    pass test Artist object includes AddAlbum, GetAlbum, and can create list of Albums
    <pagrimm@gmail.com>
    <micheal.hansen@gmail.com>

--- MusicOrganizer.Tests/ModelTests/ArtistTests.cs
+++ MusicOrganizer.Tests/ModelTests/ArtistTests.cs
@@ -12,7 +12,7 @@ namespace MusicOrganizer.Tests
     {
       Artist.ClearAll();
     }
-    
+
     [TestMethod]
     public void ArtistConstructor_CanCreateAlbumObject_MatchType()
     {
@@ -27,6 +27,18 @@ namespace MusicOrganizer.Tests
       Artist newArtist = new Artist("Radiohead");
       List<Artist> expectedList = new List<Artist> { newArtist };
       CollectionAssert.AreEqual(expectedList, Artist.GetAll());
+    }
+
+    [TestMethod]
+    public void GetAlbums_GetListOfAlbumsByArtist_List()
+    {
+      Artist newArtist = new Artist("Radiohead");
+      Album newAlbum1 = new Album("OK Computer", "Radiohead");
+      Album newAlbum2 = new Album("Kid A", "Radiohead");
+      newArtist.AddAlbum(newAlbum1);
+      newArtist.AddAlbum(newAlbum2);
+      List<Album> _expectedList = new List<Album> { newAlbum1, newAlbum2 };
+      CollectionAssert.AreEqual(_expectedList, newArtist.GetAlbums());
     }
   }
 }
--- MusicOrganizer/Models/Artist.cs
+++ MusicOrganizer/Models/Artist.cs
@@ -5,12 +5,23 @@ namespace MusicOrganizer.Models
   public class Artist
   {
     public string ArtistName { get; set; }
+    private List<Album> _albumList = new List<Album>();
     private static List<Artist> _artistList = new List<Artist>();
 
     public Artist(string artistName)
     {
       ArtistName = artistName;
       _artistList.Add(this);
+    }
+
+    public void AddAlbum(Album albumName)
+    {
+      _albumList.Add(albumName);
+    }
+
+    public List<Album> GetAlbums()
+    {
+      return _albumList;
     }
 
     public static List<Artist> GetAll()
